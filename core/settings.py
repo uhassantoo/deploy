b669463a460e9b7eb264241ac30d748f62416d6a
uhassantoor <uhassantoor@gmail.com>
2024-11-28 15:34:25 +0500

    Django

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i=io@af3ely&i$ku))bmoho4nr1+f08h)ht0@=$x1djl-ia0cx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
